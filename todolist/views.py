2bf2d28015426217a424ebb1df17908f03f4b759
Alin Babu <alinbabu2010@gmail.com>
2020-09-25 20:54:23 +0530

    Views for adding todo list and form rendering added

--- todolist/views.py
+++ todolist/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Todolist
+from .forms import TodoListForm
+from django.views.decorators.http import require_POST
 
-def index(request):
+def index(request):       # Rendering for the index page
     todo_items = Todolist.objects.order_by('id')
-    context = {'Todo_items' : todo_items}
+    form = TodoListForm()
+    context = {'Todo_items' : todo_items, 'form' : form}
     return  render(request,'todolist/index.html',context)
+
+@require_POST
+def addTodoItem(request):              # Fetching todo item and adding it to database
+    form = TodoListForm(request.POST)
+    if form.is_valid():
+        new_todo = Todolist(text=request.POST['text'])
+        new_todo.save()
+    return redirect('index')
