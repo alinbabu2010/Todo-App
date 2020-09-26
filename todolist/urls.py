2219e36c68816655a07624fd47164f08b7a8c98e
Alin Babu <alinbabu2010@gmail.com>
2020-09-26 10:02:42 +0530

    Added url path for deleting all items

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add', views.addTodoItem, name='add'),
-    path('completed/<todo_id>', views.completedTodo, name='completed')
+    path('completed/<todo_id>', views.completedTodo, name='completed'),
+    path('deletecompleted', views.deleteCompleted, name='deletecompleted'),
+    path('deleteall', views.deleteAll, name='deleteall')
 ]
