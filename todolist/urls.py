c8c1176e696f2fae34ff73fc5996e8bd942d108b
Alin Babu <alinbabu2010@gmail.com>
2020-09-26 09:30:53 +0530

    Added url path for completed item

--- todolist/urls.py
+++ todolist/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add', views.addTodoItem, name='add')
+    path('add', views.addTodoItem, name='add'),
+    path('completed/<todo_id>', views.completedTodo, name='completed')
 ]
